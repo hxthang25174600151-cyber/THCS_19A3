cb37661a07a1429d66962bdd26712c699ef11a28
hxthang25174600151-cyber <hxthang.25174600151@sv.uneti.edu.vn>
2025-12-28 20:47:55 +0700

    Delete CHUONG_12 directory

--- CHUONG_12/bai1/main.py
+++
@@ -1,4 +0,0 @@
-
-from hinh_hoc import chu_vi_hinh_vuong, chu_vi_hinh_tron
-print(chu_vi_hinh_vuong(5))
-print(chu_vi_hinh_tron(3))
--- CHUONG_12/bai3/may_tinh/nang_cao.py
+++
@@ -1,4 +0,0 @@
-
-import math
-def luy_thua(a,b): return a**b
-def can_bac_hai(x): return math.sqrt(x)
